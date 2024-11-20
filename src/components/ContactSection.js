d82eb239651189a0e0ac95f0db18fd87033e229f
Ofer Fryman <ofer@getecho.io>
2024-11-20 20:03:58 +0100

    Smaller email us

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -3,20 +3,18 @@ import React from 'react';
 
 function ContactSection() {
     return (
-        <section className="bg-white dark:bg-gray-800 pb-6">
-            <div className="my-8 text-center">
-                <span className="font-semibold text-gray-500 uppercase dark:text-gray-400">
-                    <br />
+        <section className="bg-white dark:bg-gray-800 py-4">
+            <div className="text-center">
+                <p className="font-semibold text-gray-500 uppercase dark:text-gray-400">
                     Some elements on this page are under construction!!!!
                     <br />
                     Please Contact Us
-                </span>
+                </p>
             </div>
             <div className="container mx-auto px-6">
-                <div className="flex justify-center items-center h-64">
-                    <p className="text-4xl font-extrabold text-gray-900 dark:text-white">
-                        Contact us at:                     <br />
-                        <span className="text-blue-600">info@davoice.io</span>
+                <div className="flex justify-center items-center h-16">
+                    <p className="text-xl font-extrabold text-gray-900 dark:text-white">
+                        Contact us at: <span className="text-blue-600">info@davoice.io</span>
                     </p>
                 </div>
             </div>
